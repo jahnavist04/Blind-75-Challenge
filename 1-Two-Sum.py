d1cc17c485b20f78c8233dd5173dc9c6b80ec693
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:34:41 +0530

    Remove Python code for twoSum
    Removed Python implementation of twoSum function.

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,17 +1,3 @@
-# PYTHON
-
-def twoSum(nums, target):
-    seen = {}
-
-    for i, num in enumerate(nums):
-        complement = target - num
-
-        if complement in seen:
-            return [seen[complement], i]
-
-        seen[num] = i
-
-
 #  JAVA
 import java.util.HashMap;
 
