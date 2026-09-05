66c1daf04f3e931eae533cb339aad3122d9495d8
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:35:03 +0530

    Convert maxProfit function from Python to Java
    Converted Python solution for max profit to Java.

--- 2-Best-Time-to-Buy-and-Sell-Stock.py
+++ 2-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,17 +1,4 @@
-#PYTHON 
-
-class Solution:
-    def maxProfit(self, prices):
-        min_price = prices[0]
-        max_profit = 0
-
-        for price in prices:
-            min_price = min(min_price, price)
-            max_profit = max(max_profit, price - min_price)
-
-        return max_profit
-
-  # JAVA 
+# JAVA 
 
 class Solution {
     public int maxProfit(int[] prices) {
